79cb5ae23485832a46c34a90c50632aa7c11a8dd
TheNobleOne <thenoblejmn@gmail.com>
2015-06-30 10:19:41 -0700

    ICC - Chaos
    In class code (ICC), correcting the file and saving

--- CSC-110/Chaos.py
+++ CSC-110/Chaos.py
@@ -1,1 +1,12 @@
-print ("Hello")
+#File: chaos.py
+#A simple program illustrating chaotic behaviour
+
+def main ():
+    print("This program illustrates a chaotic function")
+    x = eval (input("Enter a number between 0 and 1:"))
+    for i in range(10):
+        x = 3.9 * x * (1 - x)
+        print (x)
+
+
+main ()
