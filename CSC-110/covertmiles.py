cc4b482ad3ae44c664f0244261a9eefac1f37a77
TheNobleOne <thenoblejmn@gmail.com>
2015-07-04 23:46:29 -0700

    ICC
    Quick in class script to convert miles into km.
    
    Upgrade ideas: add inverse, add in repeatability

--- CSC-110/covertmiles.py
+++ CSC-110/covertmiles.py
@@ -3,7 +3,7 @@
 # Created by Jo Narvaez-Jensen
 
 def main ():
-    miles = eval(input("Enter the distance in miles: "))
+    miles = eval (input("Enter the distance in miles: "))
     km = 1.60934 * miles
 
     print ("The distance of", miles,"miles converts to", km, "kilometeres.")
