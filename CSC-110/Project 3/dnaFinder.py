69f33a93c0d443d6dc74ec12fbfa24a8d03d82e8
TheNobleOne <thenoblejmn@gmail.com>
2015-08-02 16:33:18 -0700

    Project 3A
    pretty simply program for searching pre-divide information, main thing
    I’m proud of putting together is line12 and correctly outputting the
    list as a string with that single line loop.

---
+++ CSC-110/Project 3/dnaFinder.py
@@ -0,0 +1,50 @@
+# dnaFinder.py
+# Created by Jo Narvaez-Jensen
+# Project 3A
+
+# This program takes in a file, creates two strings, and searches one string
+#   with the information from the other, outputting
+
+# Outputs the results of the search giving the string searched for (dna), how
+#   many times it appears (howMany) and iterates thru a list of indexes where it can be found (where)
+def printResults (dna, where, howMany):
+    print ("The DNA sequence of {0} occurs {1} times.".format (dna, howMany))
+    print ("They can be found at {0}.".format (', '.join (str (i) for i in where)))
+
+# Gets the # of times the search string appeared
+def numOfOccurance (locationList):
+    numAppear = len (locationList)
+    return numAppear
+
+# Searches for the desired string (search) within the larger string (maze), sending the index when found to a list (location)
+def finder (maze, search):
+    location = []
+    index = 0
+
+    # Loop design to also catch search matches that might overlap.
+    while index < len (maze):
+        index = maze.find (search, index)
+        if index == -1:
+            break
+        location.append (index)
+        index += 1
+    return location
+
+# Reads the file, splits the file, and assigns the values to individual
+#   strings (fieldSearched, searchTerm)
+def setData ():
+    readFile = open ('motifFinding.txt', 'r')
+    fileList = readFile.read ().rstrip (). split ('\n')
+
+    fieldSearched, searchTerm = fileList [0], fileList [1]
+
+    return fieldSearched, searchTerm
+
+# Handles the running of all funcations and passage of parameters.
+def main ():
+    fieldSearched, searchTerm = setData ()
+    locationList = finder (fieldSearched, searchTerm)
+    numAppear = numOfOccurance (locationList)
+    printResults (searchTerm, locationList, numAppear)
+
+main ()
